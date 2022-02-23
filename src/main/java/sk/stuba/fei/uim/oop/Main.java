e7f0e77740c93b5b54c7cff09afba7d05bcb3d43
SamuelHornak <xhornaks1@stuba.sk>
2022-02-23 16:33:38 +0100

    Dokumentacia ukažka

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -11,6 +11,8 @@ public class Main {
         int x = 5;
         int[] y = new int[]{1, 2, 3};
 
+        //komentare
+
         System.out.println(x);
         testPrimitive(x);
         System.out.println(x);
@@ -18,15 +20,28 @@ public class Main {
         System.out.println(Arrays.toString(y));
         testArray(y);
         System.out.println(Arrays.toString(y));
+
+
+        testArraySpread(new int[]{1, 2, 3});
+
     }
 
+    /**
+     * Táto funkcia vypisuje cislo
+     * @param cislo -integer cislo
+     */
     public static void testPrimitive(int cislo) {
         cislo = 10;
         System.out.println(cislo);
     }
 
-    public static void testArray(int @NotNull [] array) {
+    public static void testArray(int[] array) {
         array[0] = 10;
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void testArraySpread(int... array) {
+        System.out.println("text");
         System.out.println(Arrays.toString(array));
     }
 }
