2855c905b22675f06ef3a45f3eaac546332804f2
SamuelHornak <xhornaks1@stuba.sk>
2022-02-23 16:04:08 +0100

    Porovnavanie objektov

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,29 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner klavesnica = new Scanner(System.in);
+
+        String a = "a";
+        String b = "a";
+
+        System.out.printf("zadaj pismeno:");
+        String c= klavesnica.nextLine();
+
+        System.out.println("a == a:" +(a==a));
+        System.out.println("a == b:" +(a.equals(b)));
+        System.out.println("a == c:" +(a==c));
+
+        System.out.println("a.equals(a)" +(a.equals(a)));
+        System.out.println("a.equals(b)" +(a.equals(b)));
+        System.out.println("a.equals(c)" +(a.equals(c)));
+
+        System.out.println("Objects.equals(a, a):" + Objects.equals(a, a));
+        System.out.println("Objects.equals(a, a):" + Objects.equals(a, b));
+        System.out.println("Objects.equals(a, a):" + Objects.equals(a, c));
     }
 }
